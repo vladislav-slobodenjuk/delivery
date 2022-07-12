530a120f9df3d214138724c4a322072ec22f54c9
vladislav-slobodenjuk <vladjun@gmail.com>
2022-07-13 00:26:31 +0300

    components folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
